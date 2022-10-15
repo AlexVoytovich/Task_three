b981286bb10a39339866d33d1d9b576c4013271e
AlexVoyt <alexvoytovich@bk.ru>
2022-10-16 04:25:30 +0500

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Sqrt(((x1 - x2) * (x1 - x2)) + ((y1 -y2) * (y1 - y2)) + ((z1 - z2) * (z1 - z2))));

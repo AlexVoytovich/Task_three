33d745ec3480e232d1d6a4b340bf80958abc7269
AlexVoyt <alexvoytovich@bk.ru>
2022-10-17 17:20:52 +0500

    ХЗ как делать

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string str = n.ToString();
+int n1 = str[0];
+Console.WriteLine(n1);
